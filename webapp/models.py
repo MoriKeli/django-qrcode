5d4d9d5c159a8a6e3f9f21d102c78ae3b539d1ad
Mori Keli <kelimbth@gmail.com>
2022-12-06 14:05:12 +0300

    corrected a few bugs

--- webapp/models.py
+++ webapp/models.py
@@ -8,16 +8,16 @@ import qrcode
 class Products(models.Model):
     id  = models.CharField(max_length=12, primary_key=True, unique=True, editable=False)
     name = models.CharField(max_length=120, blank=False)
-    qr_code = models.ImageField(upload_to='qrcodes/', blank=False)
+    qr_code = models.ImageField(upload_to='qrcodes/', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
-    
+
 
     def __str__(self):
         return f'{self.name}'
 
     def save(self, *args, **kwargs):
-        qrcode_img = qrcode.img(self.name)
+        qrcode_img = qrcode.make(self.name)
         img_canvas = Image.new('RGB', (300, 300), 'white')
         draw = ImageDraw.Draw(img_canvas)
         img_canvas.paste(qrcode_img)
@@ -25,5 +25,5 @@ class Products(models.Model):
         buffer = BytesIO()
         img_canvas.save(buffer, 'PNG')
         self.qr_code.save(img_file_name, File(buffer), save=False)    # add save=False to avoid creation of infinite image files
-        cnavas.close()
+        img_canvas.close()
         super().save(*args, **kwargs)
