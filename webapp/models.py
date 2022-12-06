50778b94ba7dc296fabde5d8f0fc08167909ccfb
Mori Keli <kelimbth@gmail.com>
2022-12-06 13:22:47 +0300

    made a few changes

--- webapp/models.py
+++ webapp/models.py
@@ -9,6 +9,9 @@ class Products(models.Model):
     id  = models.CharField(max_length=12, primary_key=True, unique=True, editable=False)
     name = models.CharField(max_length=120, blank=False)
     qr_code = models.ImageField(upload_to='qrcodes/', blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return f'{self.name}'
@@ -24,4 +27,3 @@ class Products(models.Model):
         self.qr_code.save(img_file_name, File(buffer), save=False)    # add save=False to avoid creation of infinite image files
         cnavas.close()
         super().save(*args, **kwargs)
-        
