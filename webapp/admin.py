3af9c4b83c15e7a52752868bc63507628bbdc990
Mori Keli <kelimbth@gmail.com>
2022-12-06 13:22:34 +0300

    registered Products model

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Products
 
-# Register your models here.
+@admin.site(Products)
+class ProductsTable(admin.ModelAdmin):
+    list_display = ['name', 'created']
+    ordering = ['created']
